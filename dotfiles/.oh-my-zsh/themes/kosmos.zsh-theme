95bfb33f36a4f7e94d0cd6dbab2c9c55d2922934
DarshanSolanki27 <dsolanki2702@gmail.com>
2021-09-21 21:46:11 +0530

    Add dotfiles, add neofetch and remove discord from debian installs

---
+++ dotfiles/.oh-my-zsh/themes/kosmos.zsh-theme
@@ -0,0 +1,8 @@
+PROMPT=$'
+%{$fg_bold[green]%}%n@%m %{$reset_color%}%{$fg[blue]%}[%~]%{$reset_color%} $(git_prompt_info)
+%{$fg[yellow]%}%D{[%H:%M:%S]} %{$fg[blue]%}%(!.#.$)%{$reset_color%} '
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[red]%}("
+ZSH_THEME_GIT_PROMPT_SUFFIX=")%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[magenta]%}*%{$fg[red]%}"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
--- installer_scripts/debian_based_install.sh
+++ installer_scripts/debian_based_install.sh
@@ -1,4 +1,4 @@
-# Color
+# color
 Color_Off='\033[0m'
 Red='\033[0;31m'
 
@@ -12,21 +12,21 @@ sudo apt install vim nano curl wget apt-transport-https software-properties-comm
 
 # installs
 for package in \
-# Terminal
+# terminal
     kitty \
     zsh \
 # C/C++
     build-essential \
-# Python
+# python
     python \
     python3.9 \
-# JS
+# js
     yarn \
-# Java
+# java
     default-jdk \
-# Office
+# office
     okular \
-# Comms
+# comms
     telegram-desktop \
 # media player
     vlc \
@@ -34,7 +34,8 @@ for package in \
     ncdu \
     ranger \
     imagemagick \
-    qbittorrent
+    qbittorrent \
+    neofetch
 do
     echo -e "\n${Red} *** Installing $package *** ${Color_Off}"
     sudo apt install $package
@@ -42,7 +43,7 @@ done
 
 sudo apt update && sudo apt upgrade -y
 
-# NVM
+# nvm
 echo -e "\n${Red} *** Installing NVM *** ${Color_Off}"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install --lts
@@ -52,11 +53,11 @@ nvm use --lts
 echo -e "\n${Red} *** Installing oh-my-zsh *** ${Color_Off}"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Python setup
+# python setup
 python -m ensurepip --upgrade
 pip3 install virtualenv
 
-# VS Code
+# code
 echo -e "\n${Red} *** Installing VS Code *** ${Color_Off}"
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
@@ -65,20 +66,14 @@ rm -f packages.microsoft.gpg
 sudo apt update
 sudo apt install code
 
-# Sublime text
+# sublime text
 echo -e "\n${Red} *** Installing Sublime text *** ${Color_Off}"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install sublime-text
 
-# Discord
-echo -e "\n${Red} *** Installing Discord *** ${Color_Off}"
-wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
-sudo gdebi ~/discord.deb
-rm ~/discord.deb
-
-# Cleanup
+# cleanup
 echo -e "\n${Red} *** Cleaning up *** ${Color_Off}"
 sudo apt update && sudo apt upgrade -y
 sudo apt --fix-broken install --yes
